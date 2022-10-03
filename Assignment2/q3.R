9a7ccd15453c9d434c266d9cfbe4cd47d45be4f2
slinky55 <larry.mason@ufl.edu>
2022-10-03 00:51:17 -0400

    added volcano plot

---
+++ Assignment2/q3.R
@@ -0,0 +1,26 @@
+
+
+# this is of class DESeqResults -- we want a data frame
+deseq_df <- res %>%
+  # make into data.frame
+  as.data.frame() %>%
+  # the gene names are row names -- let's make them a column for easy display
+  tibble::rownames_to_column("Gene") %>%
+  # add a column for significance threshold results
+  dplyr::mutate(threshold = padj < 0.05) %>%
+  # sort by statistic -- the highest values will be genes with
+  # higher expression in RPL10 mutated samples
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+
+head(deseq_df)
+
+# We'll assign this as `volcano_plot`
+volcano_plot <- EnhancedVolcano::EnhancedVolcano(
+  deseq_df,
+  lab = deseq_df$Gene,
+  x = "log2FoldChange",
+  y = "padj",
+  pCutoff = 0.01 # Loosen the cutoff since we supplied corrected p-values
+)
+
+volcano_plot
