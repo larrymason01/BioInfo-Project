d09221cffc244d252ca3ecd8ab068294dd8cc65b
prin0315 <43053236+prin0315@users.noreply.github.com>
2022-10-03 00:36:18 -0400

    added umap, ***unstable***

--- Assignment2/q1.R
+++ Assignment2/q1.R
@@ -1,8 +1,9 @@
 counts_dir <- "./data/GSE127500/GSE127500_Counts_TE.txt"
 counts <- read.table(counts_dir, header = TRUE, sep = "\t")
 
+###Q1###
 ##Names rows based on gene names
-row.names(counts) <- counts$GeneId
+#row.names(counts) <- counts$GeneId
 counts <- subset(counts, select = -c(1))
 
 ##counts matrix already appears to be logarithmic
@@ -14,43 +15,52 @@ diff <- ranges[2, ] - ranges[1, ]
 plot(density(diff))
 
 
-###Q2
+###Q2###
 metadata_dir <- "./data/GSE127500/SraRunTable.txt"
 metadata <- read.csv(metadata_dir, sep=",")
 
-##Cut cDNA portion of metadata
+##clean metadata
 metadata <- subset(metadata, LibrarySelection != "cDNA")
-
-
 counts <- counts[,order(colnames(counts))]
 metadata <- metadata[order(metadata$Genotype),]
 metadata <- metadata[c(25:28,1:24),]
 rownames(metadata) <- colnames(counts)
-# colnames(counts) <- sub("_EB[0-9]+", "", colnames(counts))
-# colnames(counts) <- sub("X", "", colnames(counts))
-# colnames(counts) <- sub("Ct_D[0-9]", "Control", colnames(counts))
 
+##Deseq2
 library("DESeq2")
 dds <- DESeqDataSetFromMatrix(countData = round(counts),
                               colData = metadata,
                               design = ~ Genotype)
-dds
-
 dds <- DESeq(dds)
-resultsNames(dds) # lists the coefficients
-res <- results(dds, name="Genotype_2B_vs_1B")
+res <- results(dds, name="Genotype_comparison")
 # or to shrink log fold changes association with condition: 
-BiocManager::install("apeglm")
 res <- lfcShrink(dds, coef="Genotype_2B_vs_1B", type="apeglm")
+
 
 vsd <- vst(dds, blind=FALSE)
 rld <- rlog(dds, blind=FALSE)
-head(assay(vsd), 3)
-
 plotPCA(vsd, intgroup=c("Genotype"))
 
-library(M3C)
-data(pollen)
-tsne(pollen$data,colvec=c('gold'))
+##UMAP
+library(umap)
+library(ggplot2)
+library(magrittr)
+set.seed(12345)
+normalized_counts <- assay(vsd) %>%
+  t()
 
+umap_results <- umap::umap(normalized_counts)
+# umap_plot_df <- data.frame(umap_results$layout) %>%
+#   tibble::rownames_to_column("idname") %>%
+#   dplyr::inner_join(metadata, by = "idname")
+
+umap_plot_df <- data.frame(umap_results$layout)
+ggplot(
+  umap_plot_df,
+  aes(
+    x = X1,
+    y = X2
+  )
+) +
+  geom_point() # Plot individual points to make a scatterplot
 
