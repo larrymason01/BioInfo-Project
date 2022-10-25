509e5c701f07afcbbf290275b6d4d44df408cc2f
prin0 <prin0315@hotmail.com>
2022-10-25 16:46:12 -0400

    K-means

---
+++ Assignment3/assignment3.R
@@ -0,0 +1,58 @@
+#Directories
+counts_dir <- "./data/GSE127500/GSE127500_Counts_TE.txt"
+metadata_dir <- "./data/GSE127500/SraRunTable.txt"
+
+counts <- read.table(counts_dir, sep = "\t", row.names = 1, header=TRUE)
+metadata <- read.csv(metadata_dir, sep=",")
+
+#clean metadata file
+metadata <- subset(metadata, LibrarySelection != "cDNA")
+counts <- counts[,order(colnames(counts))]
+metadata <- metadata[order(metadata$Genotype),]
+metadata <- metadata[c(25:28,1:24),]
+rownames(metadata) <- colnames(counts)
+if (!all(rownames(metadata) == colnames(counts))) {
+  stop("Rownames != colnames")
+}
+
+#dds and filtering
+library("DESeq2")
+dds <- DESeqDataSetFromMatrix(countData = counts,
+                              colData = metadata,
+                              design = ~ Genotype)
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+#Plot PCA
+vsd <- vst(dds, blind=TRUE)
+library(apeglm)
+library(umap)
+library(magrittr)
+library(ggplot2)
+#diff analysis
+dds_diff <- DESeq(dds)
+dds_diff_results <- results(dds_diff, contrast=c("Genotype", "Control", "7B"))
+results_df <- dds_diff_results %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("Gene") %>%
+  dplyr::mutate(threshold = padj < 0.05) %>%
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+
+N = 5000
+topN <- head(results_df, N)
+topN_names <- topN$Gene
+newdata <- rowData(dds_diff)[c("baseMean", "baseVar")]
+#newdata <- topN[c("log2FoldChange", "pvalue")]
+#rownames(newdata) <- topN$Gene
+
+cols <- c("#210b1a", "#18125c", "#f54242", "#f5bc42", "#66f542", "#4287f5", "#c842f5")
+
+K <- 8
+km2 <- kmeans(newdata, K, iter.max=1000)
+library(factoextra)
+fviz_cluster(km2, data = newdata,
+             palette = cols[1:K], 
+             geom = "point",
+             ellipse.type = "convex", 
+             ggtheme = theme_bw()
+)
