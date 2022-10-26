a399bc827bd240c506f465ff124cab2b1cb36e69
prin0 <prin0315@hotmail.com>
2022-10-25 22:13:39 -0400

    Chisq

--- Assignment3/assignment3.R
+++ Assignment3/assignment3.R
@@ -45,20 +45,20 @@ topN_names <- topN$Gene
 rowdata <- rowData(dds_diff)
 
 #w/o DESeq
-newdatanls <- rowdata[c("baseMean", "baseVar")]
-newdatanls <- newdatanls[topN_names,]
-newdata <- data.frame(log(newdatanls[,1]), log(newdatanls[,2]))
-rownames(newdata) <- topN_names
-colnames(newdata) <- colnames(newdatanls)
+# newdatanls <- rowdata[c("baseMean", "baseVar")]
+# newdatanls <- newdatanls[topN_names,]
+# newdata <- data.frame(log(newdatanls[,1]), log(newdatanls[,2]))
+# rownames(newdata) <- topN_names
+# colnames(newdata) <- colnames(newdatanls)
 
 #from DESeq
-#newdata <- topN[c("log2FoldChange", "pvalue")]
-#rownames(newdata) <- topN$Gene
+newdata <- topN[c("log2FoldChange", "pvalue")]
+rownames(newdata) <- topN$Gene
 
 cols <- c("#210b1a", "#18125c", "#f54242", "#f5bc42", "#66f542", "#4287f5", "#c842f5")
 
 K <- 8
-km2 <- kmeans(newdata, K, iter.max=1000)
+km2 <- kmeans(newdata, K, iter.max=9999)
 library(factoextra)
 fviz_cluster(km2, data = newdata,
              palette = cols[1:K], 
@@ -66,3 +66,24 @@ fviz_cluster(km2, data = newdata,
              ellipse.type = "convex", 
              ggtheme = theme_bw()
 )
+
+
+###CHI SQUARED TESTING###
+top_counts <- counts[topN_names,]
+chi_table <- data.frame(matrix(0, nrow=K, ncol=28))
+colnames(chi_table) <- colnames(top_counts)
+
+cluster_vector <- km2$cluster
+
+for (rn in rownames(top_counts)) {
+  clusternum <- cluster_vector[rn]
+  chi_table[clusternum,] <- chi_table[clusternum,] + top_counts[rn,]
+}
+
+grouped_chi_table <- data.frame(matrix(0, nrow=K, ncol=8))
+colnames(grouped_chi_table) <- list("Control", "1B", "2B", "3B", "4B", "5B", "6B", "7B")
+for (cn in colnames(chi_table)) {
+  group <- metadata[cn, "Genotype"]
+  grouped_chi_table[,group] <- grouped_chi_table[,group]+ chi_table[,cn]
+}
+csq <- chisq.test(cs_group)
